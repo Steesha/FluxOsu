d36ebbd41c44ce0df7ac44bc1e67bd69cb81d501
Steesha <2327488733@qq.com>
2021-07-03 09:32:51 +0800

    Add project files.

---
+++ FluxOsu/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+
+namespace FluxOsu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Init harmony class.
+            Harmony harmony = new Harmony("today.flux.osu");
+
+        }
+    }
+}
